c2a359f1ecae6b7743d3a385980c6fecb3ab1403
mobileGALSuki <129038544+mobileGALSuki@users.noreply.github.com>
2023-03-31 13:56:15 +0900

    Update shardeum_setup.sh

--- shardeum_setup.sh
+++ shardeum_setup.sh
@@ -36,11 +36,28 @@ do
 done
 
 # 5. インストーラをダウンロードし、実行
-curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh && ./installer.sh
-until [[ "$PWD" == *"~" ]]
-do
-  sleep 1
+output = $(curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh && ./installer.sh)
+sleep 3
+y
+sleep 3
+y
+sleep 3
+echo "[$INPUT_STR]"
+sleep 3
+20000
+sleep 3
+21000
+sleep 3
+22000
+sleep 3
+echo ""
+
+# 特定の文字列が出力されるまでループ
+while [[ $output != *"operator-cli --help"* ]]; do
+  read -t 1 -n 1 # 1文字ずつ読み込む
+  output="$output$REPLY" # 読み込んだ文字を結果に追加する
 done
+sleep 10
 
 # 6. cd ~/.shardeum
 cd ~/.shardeum
