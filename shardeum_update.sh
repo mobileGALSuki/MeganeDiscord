c65ac1ba6c640dcc847e42bee661dcb9c6946741
mobileGALSuki <129038544+mobileGALSuki@users.noreply.github.com>
2023-03-31 13:18:53 +0900

    Update shardeum_update.sh

--- shardeum_update.sh
+++ shardeum_update.sh
@@ -1,27 +1,52 @@
 #!/bin/bash
 
+echo "+-------------------------------------------------------+"
+echo "パスワードを入力してください。"
+echo "+-------------------------------------------------------+"
+read -p ":" INPUT_STR
+echo "+-------------------------------------------------------+"
+echo "入力したパスワードは[$INPUT_STR]でよろしいですか？"
+echo "+-------------------------------------------------------+"
+
 # 1. cd ~/.shardeum
 cd ~/.shardeum
-sleep 5
+sleep 3
 
 # 2. ./cleanup.sh
 ./cleanup.sh
-sleep 600
+sleep 300
 
 # 3. cd ..
 cd ..
-sleep 5
+sleep 3
 
 # 4. rm -rf .shardeum
 rm -rf .shardeum
-sleep 5
+sleep 3
 
 # 5. インストーラをダウンロードし、実行
-curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh && ./installer.sh
-until [[ "$PWD" == *"~" ]]
-do
-  sleep 1
+output = $(curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh && ./installer.sh)
+sleep 3
+y
+sleep 3
+y
+sleep 3
+echo "[$INPUT_STR]"
+sleep 3
+20000
+sleep 3
+21000
+sleep 3
+22000
+sleep 3
+echo ""
+
+# 特定の文字列が出力されるまでループ
+while [[ $output != *"operator-cli --help"* ]]; do
+  read -t 1 -n 1 # 1文字ずつ読み込む
+  output="$output$REPLY" # 読み込んだ文字を結果に追加する
 done
+sleep 10
 
 # 6. cd ~/.shardeum
 cd ~/.shardeum
