6399f0fbf9c89cc2e2096ed284d667ec43234847
mobileGALSuki <129038544+mobileGALSuki@users.noreply.github.com>
2023-04-17 23:13:41 +0900

    Update shardeum_update.sh

--- shardeum_update.sh
+++ shardeum_update.sh
@@ -1,48 +1,247 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "+-------------------------------------------------------+"
-echo "パスワードを入力してください。"
-echo "+-------------------------------------------------------+"
-read -p ":" INPUT_STR
-echo "+-------------------------------------------------------+"
-echo "入力したパスワードは[$INPUT_STR]でよろしいですか？"
-echo "+-------------------------------------------------------+"
+read -s -p "Enter Password: " PASSWORD
 
-# 5. インストーラをダウンロードし、実行
-output = $(curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh && ./installer.sh)
-sleep 3
-y
-sleep 3
-y
-sleep 3
-echo "[$INPUT_STR]"
-sleep 3
-20000
-sleep 3
-21000
-sleep 3
-22000
-sleep 3
-echo ""
+set -e
 
-# 特定の文字列が出力されるまでループ
-while [[ $output != *"operator-cli --help"* ]]; do
-  read -t 1 -n 1 # 1文字ずつ読み込む
-  output="$output$REPLY" # 読み込んだ文字を結果に追加する
-done
-sleep 10
+# Check all things that will be needed for this script to succeed like access to docker and docker-compose
+# If any check fails exit with a message on what the user needs to do to fix the problem
+command -v sudo git >/dev/null 2>&1 || { echo >&2 "'git' is required but not installed."; exit 1; }
+command -v sudo docker >/dev/null 2>&1 || { echo >&2 "'docker' is required but not installed. See https://gitlab.com/shardeum/validator/dashboard/-/tree/dashboard-gui-nextjs#how-to for details."; exit 1; }
+if command -v sudo docker-compose &>/dev/null; then
+  echo "docker-compose is installed on this machine"
+elif docker --help | grep -q "compose"; then
+  echo "docker compose subcommand is installed on this machine"
+else
+  echo "docker-compose or docker compose is not installed on this machine"
+  exit 1
+fi
+
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
+
+docker-safe() {
+  if ! command -v sudo docker &>/dev/null; then
+    echo "docker is not installed on this machine"
+    exit 1
+  fi
+
+  if ! docker $@; then
+    echo "Trying again with sudo..."
+    sudo docker $@
+  fi
+}
+
+docker-compose-safe() {
+  if command -v sudo docker-compose &>/dev/null; then
+    cmd="docker-compose"
+  elif docker --help | grep -q "compose"; then
+    cmd="docker compose"
+  else
+    echo "docker-compose or docker compose is not installed on this machine"
+    exit 1
+  fi
+
+  if ! $cmd $@; then
+    echo "Trying again with sudo..."
+    sudo $cmd $@
+  fi
+}
+
+get_ip() {
+  local ip
+  if command -v ip >/dev/null; then
+    ip=$(ip addr show $(ip route | awk '/default/ {print $5}') | awk '/inet/ {print $2}' | cut -d/ -f1 | head -n1)
+  elif command -v netstat >/dev/null; then
+    # Get the default route interface
+    interface=$(netstat -rn | awk '/default/{print $4}' | head -n1)
+    # Get the IP address for the default interface
+    ip=$(ifconfig "$interface" | awk '/inet /{print $2}')
+  else
+    echo "Error: neither 'ip' nor 'ifconfig' command found. Submit a bug for your OS."
+    return 1
+  fi
+  echo $ip
+}
+
+get_external_ip() {
+  external_ip=''
+  external_ip=$(curl -s https://api.ipify.org)
+  if [[ -z "$external_ip" ]]; then
+    external_ip=$(curl -s http://checkip.dyndns.org | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+  fi
+  if [[ -z "$external_ip" ]]; then
+    external_ip=$(curl -s http://ipecho.net/plain)
+  fi
+  if [[ -z "$external_ip" ]]; then
+    external_ip=$(curl -s https://icanhazip.com/)
+  fi
+    if [[ -z "$external_ip" ]]; then
+    external_ip=$(curl --header  "Host: icanhazip.com" -s 104.18.114.97)
+  fi
+  if [[ -z "$external_ip" ]]; then
+    external_ip=$(get_ip)
+    if [ $? -eq 0 ]; then
+      echo "The IP address is: $IP"
+    else
+      external_ip="localhost"
+    fi
+  fi
+  echo $external_ip
+}
+
+if [[ $(docker-safe info 2>&1) == *"Cannot connect to the Docker daemon"* ]]; then
+    echo "Docker daemon is not running"
+    exit 1
+else
+    echo "Docker daemon is running"
+fi
+
+cat << EOF
+
+#########################
+# 0. GET INFO FROM USER #
+#########################
+
+EOF
+
+echo -e "\e[1m\e[32m RUNDASHBOARD = y \e[0m" && sleep 1
+
+RUNDASHBOARD="y"
+
+echo -e "\e[1m\e[32m PASSWORD = $PASSWORD \e[0m" && sleep 1
+
+DASHPASS="$PASSWORD"
+
+echo -e "\e[1m\e[32m Port Settings \e[0m" && sleep 1
+DASHPORT="20000"
+SHMEXT="21000"
+SHMINT="22000"
+NODEHOME=~/.shardeum
+
+APPSEEDLIST="archiver-sphinx.shardeum.org"
+APPMONITOR="monitor-sphinx.shardeum.org"
+
+cat <<EOF
+
+###########################
+# 1. Pull Compose Project #
+###########################
+
+EOF
+
+if [ -d "$NODEHOME" ]; then
+  if [ "$NODEHOME" != "$(pwd)" ]; then
+    echo "Removing existing directory $NODEHOME..."
+    rm -rf "$NODEHOME"
+  else
+    echo "Cannot delete current working directory. Please move to another directory and try again."
+  fi
+fi
+
+git clone https://gitlab.com/shardeum/validator/dashboard.git ${NODEHOME} &&
+  cd ${NODEHOME} &&
+  chmod a+x ./*.sh
+
+cat <<EOF
+
+###############################
+# 2. Create and Set .env File #
+###############################
+
+EOF
+
+SERVERIP=$(get_external_ip)
+LOCALLANIP=$(get_ip)
+cd ${NODEHOME} &&
+touch ./.env
+cat >./.env <<EOL
+APP_IP=auto
+EXISTING_ARCHIVERS=[{"ip":"18.194.3.6","port":4000,"publicKey":"758b1c119412298802cd28dbfa394cdfeecc4074492d60844cc192d632d84de3"},{"ip":"139.144.19.178","port":4000,"publicKey":"840e7b59a95d3c5f5044f4bc62ab9fa94bc107d391001141410983502e3cde63"},{"ip":"139.144.43.47","port":4000,"publicKey":"7af699dd711074eb96a8d1103e32b589e511613ebb0c6a789a9e8791b2b05f34"},{"ip":"72.14.178.106","port":4000,"publicKey":"2db7c949632d26b87d7e7a5a4ad41c306f63ee972655121a37c5e4f52b00a542"}]
+APP_MONITOR=${APPMONITOR}
+DASHPASS=${DASHPASS}
+DASHPORT=${DASHPORT}
+SERVERIP=${SERVERIP}
+LOCALLANIP=${LOCALLANIP}
+SHMEXT=${SHMEXT}
+SHMINT=${SHMINT}
+EOL
+
+cat <<EOF
+
+##########################
+# 3. Clearing Old Images #
+##########################
+
+EOF
+
+./cleanup.sh
+
+cat <<EOF
+
+##########################
+# 4. Building base image #
+##########################
+
+EOF
+
+cd ${NODEHOME} &&
+docker-safe build --no-cache -t local-dashboard -f Dockerfile --build-arg RUNDASHBOARD=${RUNDASHBOARD} .
+
+cat <<EOF
+
+############################
+# 5. Start Compose Project #
+############################
+
+EOF
+
+cd ${NODEHOME}
+if [[ "$(uname)" == "Darwin" ]]; then
+  sed "s/- '8080:8080'/- '$DASHPORT:$DASHPORT'/" docker-compose.tmpl > docker-compose.yml
+  sed -i '' "s/- '9001-9010:9001-9010'/- '$SHMEXT:$SHMEXT'/" docker-compose.yml
+  sed -i '' "s/- '10001-10010:10001-10010'/- '$SHMINT:$SHMINT'/" docker-compose.yml
+else
+  sed "s/- '8080:8080'/- '$DASHPORT:$DASHPORT'/" docker-compose.tmpl > docker-compose.yml
+  sed -i "s/- '9001-9010:9001-9010'/- '$SHMEXT:$SHMEXT'/" docker-compose.yml
+  sed -i "s/- '10001-10010:10001-10010'/- '$SHMINT:$SHMINT'/" docker-compose.yml
+fi
+./docker-up.sh
+
+echo "Starting image. This could take a while..."
+(docker-safe logs -f shardeum-dashboard &) | grep -q 'done'
+
+#Do not indent
+if [ $RUNDASHBOARD = "y" ]
+then
+cat <<EOF
+  To use the Web Dashboard:
+    1. Note the IP address that you used to connect to the node. This could be an external IP, LAN IP or localhost.
+    2. Open a web browser and navigate to the web dashboard at https://<Node IP address>:$DASHPORT
+    3. Go to the Settings tab and connect a wallet.
+    4. Go to the Maintenance tab and click the Start Node button.
+
+  If this validator is on the cloud and you need to reach the dashboard over the internet,
+  please set a strong password and use the external IP instead of localhost.
+EOF
+fi
+
+cat <<EOF
+
+To use the Command Line Interface:
+	1. Navigate to the Shardeum home directory ($NODEHOME).
+	2. Enter the validator container with ./shell.sh.
+	3. Run "operator-cli --help" for commands
+
+EOF
 
 # 6. cd ~/.shardeum
-cd ~/.shardeum
-sleep 5
+echo -e "\e[1m\e[32m6. cd ~/.shardeum \e[0m" && sleep 1
+cd ~/.shardeum && echo "Update 6 has completed"
 
 # 7. ./shell.sh
-./shell.sh
-sleep 5
+echo -e "\e[1m\e[32m7. ./shell.sh \e[0m" && sleep 1
+./shell.sh && echo "Update 7 has completed" && sleep 2
 
 # 8. operator-cli gui start
-operator-cli gui start
-sleep 5
-
-# 完了メッセージを表示
-echo "The macro has been executed successfully."
+echo -e "\e[1m\e[32m8. operator-cli gui start \e[0m" && sleep 1
+operator-cli gui start && echo "Update 8 has completed"
