f1ed638cdaabf6097df593004acff659471f509a
mobileGALSuki <129038544+mobileGALSuki@users.noreply.github.com>
2023-04-17 09:50:33 +0900

    Update shardeum_setup.sh

--- shardeum_setup.sh
+++ shardeum_setup.sh
@@ -19,7 +19,7 @@ sleep 15
 sudo chmod +x /usr/local/bin/docker-compose && echo "Update 4-1 has completed"
 
 # 5. インストーラをダウンロードし、実行
-curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh && ./installer.sh 
+output = $(curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh && ./installer.sh)
 sleep 15
 y
 sleep 10
