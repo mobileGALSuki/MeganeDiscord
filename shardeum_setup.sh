a75572ae9032a9e9e2925001a864997cec12dcaf
mobileGALSuki <129038544+mobileGALSuki@users.noreply.github.com>
2023-04-17 14:17:16 +0900

    Update shardeum_setup.sh

--- shardeum_setup.sh
+++ shardeum_setup.sh
@@ -3,23 +3,29 @@
 read -s -p "Enter Password: " PASSWORD
 
 # 1. Update 1
+echo -e "\e[1m\e[32m1. sudo apt-get install curl -y \e[0m" && sleep 1
 sudo apt-get install curl -y && echo "Update 1 has completed"
 
 # 2. Update 2
+echo -e "\e[1m\e[32m2. sudo apt update -y \e[0m" && sleep 1
 sudo apt update -y && echo "Update 2 has completed"
 
 # 3. Update 3
+echo -e "\e[1m\e[32m3. sudo apt install docker.io -y \e[0m" && sleep 1
 sudo apt install docker.io -y && echo "Update 3 has completed"
 
 # 4. Update 4
+echo -e "\e[1m\e[32m4. sudo curl -L \e[0m" && sleep 1
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose -y && echo "Update 4 has completed"
 sleep 15
 
 # 4. Update 4-1
+echo -e "\e[1m\e[32m5. sudo chmod +x \e[0m" && sleep 1
 sudo chmod +x /usr/local/bin/docker-compose && echo "Update 4-1 has completed"
 
 # 5. インストーラをダウンロードし、実行
-output = $(curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh && ./installer.sh)
+echo -e "\e[1m\e[32m6. Install Process Started \e[0m" && sleep 1
+curl -O https://gitlab.com/shardeum/validator/dashboard/-/raw/main/installer.sh && chmod +x installer.sh && ./installer.sh
 sleep 10
 echo "y"
 sleep 10
@@ -36,11 +42,11 @@ sleep 3
 echo ""
 
 # 特定の文字列が出力されるまでループ
-while [[ $output != *"operator-cli --help"* ]]; do
-  read -t 1 -n 1 # 1文字ずつ読み込む
-  output="$output$REPLY" # 読み込んだ文字を結果に追加する
-done
-sleep 3
+#while [[ $output != *"operator-cli --help"* ]]; do
+#  read -t 1 -n 1 # 1文字ずつ読み込む
+#  output="$output$REPLY" # 読み込んだ文字を結果に追加する
+#done
+#sleep 3
 
 # 6. cd ~/.shardeum
 cd ~/.shardeum && echo "Update 6 has completed"
