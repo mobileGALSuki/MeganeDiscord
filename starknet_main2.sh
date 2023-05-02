5df38dcd6f71a1c29f4c04cd25ce60ab523da559
mobileGALSuki <129038544+mobileGALSuki@users.noreply.github.com>
2023-05-03 01:23:58 +0900

    Add files via upload

--- starknet_main2.sh
+++ starknet_main2.sh
@@ -23,9 +23,7 @@ sudo apt install screen -y
 
 git clone --branch v0.5.3 https://github.com/eqlabs/pathfinder.git
 
-screen -S node
-
 sudo apt install docker.io -y
 
 mkdir -p $HOME/pathfinder
-docker run --rm -p 9545:9545 --user "$(id -u):$(id -g)" -e RUST_LOG=info -e PATHFINDER_ETHEREUM_API_URL=$ALCHEMY -v $HOME/pathfinder:/usr/share/pathfinder/data eqlabs/pathfinder
+screen -S starknet -d -m docker run --rm -p 9545:9545 --user "$(id -u):$(id -g)" -e RUST_LOG=info -e PATHFINDER_ETHEREUM_API_URL=$ALCHEMY -v $HOME/pathfinder:/usr/share/pathfinder/data eqlabs/pathfinder
