292590abc265362c150aacf18855d4c2c3002632
mobileGALSuki <129038544+mobileGALSuki@users.noreply.github.com>
2023-05-04 22:20:07 +0900

    Update shardeum_update.sh

--- shardeum_update.sh
+++ shardeum_update.sh
@@ -190,14 +190,8 @@ RUNDASHBOARD="y"
 CHANGEPASSWORD="y"
 DASHPASS=PASSWORD
 
-  # Hash the password using the fallback mechanism
-  DASHPASS=$(hash_password "$DASHPASS")
-else
-  DASHPASS=$PREVIOUS_PASSWORD
-  if ! [[ $DASHPASS =~ ^[0-9a-f]{64}$ ]]; then
-    DASHPASS=$(hash_password "$DASHPASS")
-  fi
-fi
+# Hash the password using the fallback mechanism
+DASHPASS=$(hash_password "$DASHPASS")
 
 if [ -z "$DASHPASS" ]; then
   echo -e "\nFailed to hash the password. Please ensure you have openssl"
